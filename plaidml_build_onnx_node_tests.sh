d1ba7f45b4f527782d00ca6160c4f9d32c046b3a
Sana Nazir <snazir@kmacaddi-MOBL1.amr.corp.intel.com>
2020-09-28 15:28:53 -0700

    removing mac build support from pipeline script

--- plaidml_build_onnx_node_tests.sh
+++ plaidml_build_onnx_node_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# This is a temporary file created for a Gitlab pipeline
+# This is a temporary file created for a Github pipeline
 # This file will be deleted once this branch is finalized 
 
 # This file contains commands that build onnxruntime with plaidml execution 
@@ -27,23 +27,13 @@ then
     conda deactivate
     export TODO_TEMP_PLAIDML_DIR=$PWD
     export TODO_TEMP_PLAIDML_LIB_DIR=$PWD/bazel-bin/plaidml/libplaidml.so
-elif [[ "$OSTYPE" == "darwin"* ]]; 
-then
+else
 # TODO (PlaidML): dylib is no longer produced for plaidml through the bazel 
 # build setup ('bazelisk build //plaidml:plaidml' previously shlib) (see pull 1335)
 # fix required to get this working on mac again 
-# for now going back in time for development ease on mac. 
-# Any plaidml testing done on mac using this setup is now obsolete unless retested on 
-# linux with latest plaidml build 
-    git checkout 236c0c44d985d320e36f208c8e5f3c671996b27a
-    ./configure
-    conda activate .cenv/
-    bazelisk build //plaidml:shlib
-    conda deactivate
-    export TODO_TEMP_PLAIDML_DIR=$PWD
-    export TODO_TEMP_PLAIDML_LIB_DIR=$PWD/bazel-bin/plaidml/libplaidml.dylib
+# to run locally on mac use plaidml-v1 commit #(236c0c44d985d320e36f208c8e5f3c671996b27a)
+    echo "Error: This buld required Linux"
 fi
-
 cd ../../
 # Build onnxruntime with plaidml execution provider support 
 ./build.sh --config RelWithDebInfo --build_shared_lib --parallel --use_plaidml
