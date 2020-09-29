39409c4a0a94f34a4d51aa775792cbc2b70bbdea
Sana Nazir <snazir@snazir-mac01.local>
2020-09-29 15:38:14 -0700

    fix: The case of the missing lib on mac

--- plaidml_build_onnx_node_tests.sh
+++ plaidml_build_onnx_node_tests.sh
@@ -17,23 +17,21 @@ START=$(date +%s)
 
 git clone --recursive --branch plaidml-v1 https://github.com/plaidml/plaidml.git ./build/plaidml
 cd build/plaidml/
-
+./configure
+conda activate .cenv/
+bazelisk build //plaidml:plaidml
+conda deactivate
 # Set environment variables so that onnxruntime can find plaidml 
+export TODO_TEMP_PLAIDML_DIR=$PWD
+
 if [[ "$OSTYPE" == "linux-gnu"* ]]; 
 then
-    ./configure
-    conda activate .cenv/
-    bazelisk build //plaidml:plaidml
-    conda deactivate
-    export TODO_TEMP_PLAIDML_DIR=$PWD
-    export TODO_TEMP_PLAIDML_LIB_DIR=$PWD/bazel-bin/plaidml/libplaidml.so
-else
-# TODO (PlaidML): dylib is no longer produced for plaidml through the bazel 
-# build setup ('bazelisk build //plaidml:plaidml' previously shlib) (see pull 1335)
-# fix required to get this working on mac again 
-# to run locally on mac use plaidml-v1 commit #(236c0c44d985d320e36f208c8e5f3c671996b27a)
-    echo "Error: This buld requires Linux"
+  export TODO_TEMP_PLAIDML_LIB_DIR=$PWD/bazel-bin/plaidml/libplaidml.so
+elif [[ "$OSTYPE" == "darwin"* ]]; 
+then
+  export TODO_TEMP_PLAIDML_LIB_DIR=$PWD/bazel-out/darwin_x86_64-opt/bin/plaidml/libplaidml.dylib  
 fi
+
 cd ../../
 # Build onnxruntime with plaidml execution provider support 
 ./build.sh --config RelWithDebInfo --build_shared_lib --parallel --use_plaidml
